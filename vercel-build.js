882ff7cbb21daf9e2bb1cc2a96e9fd0c941af364
williamfohara <billlyoharaa@gmail.com>
2025-07-03 15:41:31 -0500

    idk aagin

--- vercel-build.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require("child_process");
-
-console.log("🗂 File structure at build time:");
-
-const output = execSync(
-  `find . -type f \\( -path "./node_modules/*" -o -path "./.git/*" -o -path "./.vercel/*" \\) -prune -false -o -print`
-);
-
-console.log(output.toString());
